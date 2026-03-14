7152601e3a1558ae8ea772a2a12405965efd79d2
Heochan1125 <geehee1125@gmail.com>
2026-03-14 17:38:05 +0900

    spring boot first commit
    backend 에 관한 first init 입니다.

---
+++ back/commerce_spring_api/src/main/java/com/example/commerce_spring_api/CommerceSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.commerce_spring_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceSpringApiApplication.class, args);
+	}
+
+}
---
+++ back/commerce_spring_api/src/test/java/com/example/commerce_spring_api/CommerceSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commerce_spring_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
