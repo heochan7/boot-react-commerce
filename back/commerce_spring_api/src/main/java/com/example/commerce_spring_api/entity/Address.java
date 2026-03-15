4e56fa33d6a161733011ee285caea549f1cd90f0
Heochan1125 <geehee1125@gmail.com>
2026-03-15 17:19:33 +0900

    Entity ( Address, Seller ) 변경
    Entity (address, seller ) 변경 수정 필요

--- back/commerce_spring_api/src/main/java/com/example/commerce_spring_api/entity/Address.java
+++ back/commerce_spring_api/src/main/java/com/example/commerce_spring_api/entity/Address.java
@@ -1,17 +1,31 @@
 package com.example.commerce_spring_api.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
-
-public class Address {
+@Getter
+public class Address extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
+    @Column(nullable = false)
+    private String recipientName;
 
+    @Column(nullable = false)
+    private String zipCode;
 
+    @Column(nullable = false)
+    private String baseAddress;
+
+    private String detailAddress;
+
+    private Boolean isDefault = false;
 }
