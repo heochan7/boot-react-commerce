8b5f6c9e658003313bf251b50b84c9789a4ef7a9
Heochan1125 <110871957+heochan7@users.noreply.github.com>
2026-03-15 21:53:07 +0900

    category Entity
    category Entity 추가

---
+++ back/commerce_spring_api/src/main/java/com/example/commerce_spring_api/entity/product/Category.java
@@ -0,0 +1,30 @@
+package com.example.commerce_spring_api.entity.product;
+
+import com.example.commerce_spring_api.entity.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent_id;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children = new ArrayList<>();
+}
