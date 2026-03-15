472969ee0a8bc63a0db6c00d9dffcfbc7aef3793
Heochan1125 <geehee1125@gmail.com>
2026-03-15 17:18:47 +0900

    BaseEntity 변경
    BaseEntity 에 Getter 를 추가하고, 접근 제어자 protected 로 변경

--- back/commerce_spring_api/src/main/java/com/example/commerce_spring_api/entity/BaseEntity.java
+++ back/commerce_spring_api/src/main/java/com/example/commerce_spring_api/entity/BaseEntity.java
@@ -3,6 +3,9 @@ package com.example.commerce_spring_api.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,6 +13,8 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BaseEntity {
     @CreatedDate
     @Column(updatable = false)
