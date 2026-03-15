a3716a8ca4b6f3c24f99f42e8775b1aeb25c21b5
Heochan1125 <geehee1125@gmail.com>
2026-03-15 17:04:34 +0900

    User Entity
    User Entity 에 Getter 추가

--- back/commerce_spring_api/src/main/java/com/example/commerce_spring_api/entity/User.java
+++ back/commerce_spring_api/src/main/java/com/example/commerce_spring_api/entity/User.java
@@ -1,11 +1,13 @@
 package com.example.commerce_spring_api.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@NoArgsConstructor
 @Table(name="users")
+@Getter
+@NoArgsConstructor
 public class User extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
